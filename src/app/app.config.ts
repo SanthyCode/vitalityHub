ffb187c369d1593c4db7831a2223e66a7b4dd95a
SantyCode <santy.pulido28@gmail.com>
2026-04-08 08:29:05 -0500

    fix: intento de correcion en rutas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router'; // 1. Agregas withHashLocation aquí
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withHashLocation()) 
+  ]
 };
