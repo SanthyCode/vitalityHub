5dbc56abb50602db5cbaace15a3f2a7853512eea
SantyCode <santy.pulido28@gmail.com>
2026-04-08 07:43:17 -0500

    Se crea pagina de art3fin, para su venta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Artr3finComponent } from './pages/artr3fin/artr3fin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'artr3fin',
+        component: Artr3finComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
